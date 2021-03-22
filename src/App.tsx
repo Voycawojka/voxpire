2871c589055bbf06c9ffcacd51a829ace778fa00
Voycawojka <filip.artur.kowalski@gmail.com>
2021-03-22 18:37:58 +0100

    Introduce styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,42 @@
 import React, { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
+import { AppContainer, Button, Header, Link, Logo } from './styled'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppContainer>
+      <Header>
+        <Logo src={logo} alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button onClick={() => setCount((count) => count + 1)}>
+          <Button onClick={() => setCount((count) => count + 1)}>
             count is: {count}
-          </button>
+          </Button>
         </p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a
-            className="App-link"
+          <Link
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </Link>
           {' | '}
-          <a
-            className="App-link"
+          <Link
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
           >
             Vite Docs
-          </a>
+          </Link>
         </p>
-      </header>
-    </div>
+      </Header>
+    </AppContainer>
   )
 }
 
